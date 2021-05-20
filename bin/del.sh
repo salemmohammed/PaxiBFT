59ae1a6bc18a2e81f26d2b5c9ce0fa3154c5d216
salemmohammed <salemmoh@buffalo.edu>
2021-05-20 00:47:19 -0400

    bin

--- bin/del.sh
+++ bin/del.sh
@@ -2,5 +2,11 @@ kill -9 $(lsof -i:1735 -t)
 kill -9 $(lsof -i:1736 -t)
 kill -9 $(lsof -i:1737 -t)
 kill -9 $(lsof -i:1738 -t)
+kill -9 $(lsof -i:1739 -t)
+kill -9 $(lsof -i:1740 -t)
+kill -9 $(lsof -i:1741 -t)
+kill -9 $(lsof -i:1742 -t)
+kill -9 $(lsof -i:1743 -t)
+kill -9 $(lsof -i:1744 -t)
 rm client* server* cmd history* latency
 
